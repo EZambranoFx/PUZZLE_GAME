da58b12645c72abf1d24b206a40df3739fb5b6c0
Christian Bueno <chmabuen@espol.edu.ec>
2024-07-31 12:52:31 -0500

    refactor: create grid of images

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,22 +5,91 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    // Start is called before the first frame update
+    [Header("Game Elements")]
+    [Range(2, 6)]
+    [SerializeField] private int difficulty = 4;
+    [SerializeField] private Transform gameHolder;
+    [SerializeField] private Transform piecePrefab;
+    
     [Header("UI Elements")]
     [SerializeField] private List<Texture2D> imageTextures;
     [SerializeField] private Transform levelSelectPanel;
     [SerializeField] private Image levelSelectedPrefab;
+
+    private List<Transform> pieces;
+    private Vector2Int dimensions;
+    private float width;
+    private float height;
+    
     void Start()
     {
-        foreach (Texture2D texture in imageTextures)
-        {
+        foreach (Texture2D texture in imageTextures) {
             Image image = Instantiate(levelSelectedPrefab, levelSelectPanel);
-            image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+            image.sprite = Sprite.Create(texture, new Rect(0f, 0f, texture.width, texture.height), Vector2.zero);
+            image.GetComponent<Button>().onClick.AddListener(delegate { StartGame(texture); });
+        }
+        
+    }
 
+    public void StartGame(Texture2D jigsawTexture) {
+        levelSelectPanel.gameObject.SetActive(false);
+        pieces = new List<Transform>();
+
+        dimensions = GetDimensions(jigsawTexture, difficulty);
+
+        CreateJigsawPieces(jigsawTexture);
+    }
+
+    Vector2Int GetDimensions(Texture2D jigsawTexture, int difficulty) {
+        Vector2Int dimensions = Vector2Int.zero;
+        if (jigsawTexture.width < jigsawTexture.height) {
+            dimensions.x = difficulty;
+            dimensions.y = (difficulty * jigsawTexture.height) / jigsawTexture.width;
+        } else {
+            dimensions.x = (difficulty * jigsawTexture.width) / jigsawTexture.height;
+            dimensions.y = difficulty;
+        }
+
+        return dimensions;
+    }
+
+    void CreateJigsawPieces(Texture2D jigsawTexture) {
+        height = 1f / dimensions.y;
+        float aspect = (float)jigsawTexture.width / jigsawTexture.height;
+        width = aspect / dimensions.x;
+
+        for (int row = 0; row < dimensions.y; row++) {
+            for (int col = 0; col < dimensions.x; col++) {
+                Transform piece = Instantiate(piecePrefab, gameHolder);
+                piece.localPosition = new Vector3(
+                    (-width * dimensions.x / 2) + (width * col) + (width / 2),
+                    (-height * dimensions.y / 2) + (height * row) + (height / 2),
+                    -1
+                );
+
+                piece.localScale = new Vector3(width, height, 1f);
+
+                piece.name = $"Piece {(row * dimensions.x) + col}";
+                pieces.Add(piece);
+
+                float width1 = 1f /dimensions.x;
+                float height1 = 1f / dimensions.y;
+                Vector2[] uv = new Vector2[4];
+                uv[0] = new Vector2(width1 * col, height1 * row);
+                uv[1] = new Vector2(width1 * (col + 1), height1 * row);
+                uv[2] = new Vector2(width1 * col, height1 * (row + 1));
+                uv[3] = new Vector2(width1 * (col + 1), height1 * (row + 1));
+                Mesh mesh = piece.GetComponent<MeshFilter>().mesh;
+                mesh.uv = uv;
+
+                piece.GetComponent<MeshRenderer>().material.SetTexture("_MainTex", jigsawTexture);
+            }
         }
     }
 
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    void Update()
     {
         
     }
